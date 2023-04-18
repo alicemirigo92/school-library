81426b136c371e8d6e9d458355d99550b2245f61
alicemirigo92 <alicemirigo92@gmai.com>
2023-04-18 16:41:06 +0300

    Add files

--- person.rb
+++ person.rb
@@ -1,21 +1,12 @@
-class Person 
+class Person
   attr_accessor :name, :age
   attr_reader :id
 
-
-  def initialize(age, name = "Unknown", parent_permission = true)
-    @id = rand(1..1000)
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
-  end
-
-  def name=(name)
-    @name = name
-  end
-
-  def age=(age)
-    @age = age
   end
 
   def can_use_services?

be5884467ecd302adc27699e22dd16e7840d864b
alicemirigo92 <alicemirigo92@gmai.com>
2023-04-19 19:18:54 +0300

    Fix error

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -4,7 +4,10 @@ class TrimmerDecorator < Decorator
   MAXIMUM_NAME_LENGTH = 10
 
   def correct_name
-    name = @nameable.correct_name
-    @nameable.correct_name.length > MAXIMUM_NAME_LENGTH ? @nameable.correct_name[0...MAXIMUM_NAME_LENGTH] : @nameable.correct_name
+    if @nameable.correct_name.length > MAXIMUM_NAME_LENGTH
+      @nameable.correct_name[0...MAXIMUM_NAME_LENGTH]
+    else
+      @nameable.correct_name
+    end
   end
 end

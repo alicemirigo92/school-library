e33d6132a251a7a9b1a0d0465cf232c6774b9c8a
alicemirigo92 <alicemirigo92@gmai.com>
2023-04-19 16:17:05 +0300

    Turn my  Person class to Nameable

--- person.rb
+++ person.rb
@@ -1,27 +1,27 @@
-# require './nameable'
+require './nameable'
 
-# class Person < Nameable
-#   def initialize(age, name, parent_permission: true)
-#     super()
-#     @age = age
-#     @name = name
-#     @parent_permission = parent_permission
-#   end
+class Person < Nameable
+  def initialize(age, name, parent_permission: true)
+    super()
+    @age = age
+    @name = name
+    @parent_permission = parent_permission
+  end
 
-#   attr_reader :id
-#   attr_accessor :name, :age
+  attr_reader :id
+  attr_accessor :name, :age
 
-#   def correct_name
-#     @name
-#   end
+  def correct_name
+    @name
+  end
 
-#   private
+  private
 
-#   def is_of_age?
-#     @age >= 18
-#   end
+  def is_of_age?
+    @age >= 18
+  end
 
-#   def can_use_services?
-#     @parent_permission || is_of_age?
-#   end
-# end
+  def can_use_services?
+    @parent_permission || is_of_age?
+  end
+end

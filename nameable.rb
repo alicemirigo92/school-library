7a76a3d3d3b82b4863aede01be12e24e978e388c
alicemirigo92 <alicemirigo92@gmai.com>
2023-04-19 16:13:58 +0300

    Create a class Nameable

---
+++ nameable.rb
@@ -0,0 +1,5 @@
+class Nameable
+  def correct_name
+    raise NotImplementedError, 'Correct name must be implemented'
+  end
+end
---
+++ trimmer_decorator.rb
@@ -0,0 +1,10 @@
+# require './decorator'
+
+# class TrimmerDecorator < Decorator
+#   MAXIMUM_NAME_LENGTH = 10
+
+#   def correct_name
+#     # name = @nameable.correct_name
+#     name.length > MAXIMUM_NAME_LENGTH ? name[0...MAXIMUM_NAME_LENGTH] : name
+#   end
+# end
